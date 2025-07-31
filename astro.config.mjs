1824f618e3abaee56350e05cff393de940aae242
Saeed24Shariq <shariqoffice01@gmail.com>
2025-07-31 23:15:21 +0500

    base added to the gihub...

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astronaut.github.io',
+  base: 'New-Portfolio',
   adapter: node({
     mode: 'standalone'
   })

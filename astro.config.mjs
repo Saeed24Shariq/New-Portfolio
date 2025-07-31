8c4075016826926cbb026433103da3570fa466b4
Saeed24Shariq <shariqoffice01@gmail.com>
2025-07-31 23:00:38 +0500

    Build Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Saeed24Shariq.github.io',
   adapter: node({
     mode: 'standalone'
   })

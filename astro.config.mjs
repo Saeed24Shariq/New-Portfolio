887529a21634b5b724aa59298bfa35d05c1fc28c
Saeed24Shariq <shariqoffice01@gmail.com>
2025-07-31 23:11:09 +0500

    deployment files added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astronaut.github.io',
   adapter: node({
     mode: 'standalone'
   })
